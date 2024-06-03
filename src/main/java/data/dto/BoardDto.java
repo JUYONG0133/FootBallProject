a4ae1ee3dfe3f314ff4e187da9f054fcbe4e128b
LBang98 <dlqudgus98@gmail.com>
2024-06-03 15:36:23 +0900

    set

---
+++ src/main/java/data/dto/BoardDto.java
@@ -0,0 +1,15 @@
+package data.dto;
+
+import java.sql.Timestamp;
+
+public class BoardDto {
+
+    private String id;
+    private String title;
+    private String content;
+    private String photo;
+    private int like;
+    private int unlike;
+    private Timestamp writeday;
+
+}
