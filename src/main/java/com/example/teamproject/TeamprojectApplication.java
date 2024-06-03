aba29c90e968a43c381b1a38f1c58682d0262ce7
LBang98 <dlqudgus98@gmail.com>
2024-06-03 16:45:53 +0900

    set

--- src/main/java/com/example/teamproject/TeamprojectApplication.java
+++ src/main/java/com/example/teamproject/TeamprojectApplication.java
@@ -1,13 +1,14 @@
 package com.example.teamproject;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"controller","data.*","tiles.config","com.example.teamproject","naver.cloud"})
-
+@ComponentScan({"controller.*","data.*","tiles.config","com.example.teamproject","naver.cloud"})
+@MapperScan("data.mapper")
 public class TeamprojectApplication {
 
 	public static void main(String[] args) {
